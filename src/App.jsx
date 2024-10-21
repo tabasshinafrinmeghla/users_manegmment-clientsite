ab28061337045b824f1583249496effebf76a42e
tabasshinafrinmeghla <tabasshinafrinmeghla@gamil.com>
2024-10-21 17:37:08 +0600

    added

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,62 @@ import './App.css'
 
 function App() {
   
-  const [users, setUsers] =useState([]);
+  const [users, setUsers] = useState ([]);
 
   useEffect(()=>{
     fetch ('http://localhost:5000/users')
     .then(res => res.json())
     .then(data => setUsers(data));
-  },[])
+  }, [])
+
+
+const handleAddUser = event =>{
+event.preventDefault();
+const form =event.target;
+const name =form.Name.value;
+const email=form.Email.value;
+const user ={ name ,email}
+console.log(user);
+fetch('http://localhost:5000/users',{
+  method: 'POST',
+  headers: {
+    'content-type' : "application/json"
+  },
+  body:JSON.stringify(user)
+} )
+.then(res => res.json())
+.then(data => {
+  console.log(data);
+  
+const newUsers = [... users, data]
+setUsers(newUsers);
+form.reset();
+
+})
+
+  }
 
   return (
     <>
       
       <h1>User Manegment Clientsite</h1>
-      <p>Number of Users:{users.length}</p>
-      
+      <p>Number of Users: {users.length}</p>
+
+<form onSubmit={handleAddUser}>
+  <input type="text" name="Name" id="" />
+  <br />
+  <input type="email" name="Email" id="" />
+  <br />
+  <input type="submit" name="add user" id="" />
+ 
+</form>
+
+      <div>
+        {
+          users.map(user => <p key={user.id}> {user.id} {user.name} - {user.email}</p>)
+        }
+      </div>
+
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,22 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App> ,
+  },
+]);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )

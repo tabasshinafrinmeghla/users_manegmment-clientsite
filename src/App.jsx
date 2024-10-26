7a04138de1d9adece66f339c8c506eb3bb8826dd
tabasshinafrinmeghla <tabasshinafrinmeghla@gamil.com>
2024-10-26 17:44:48 +0600

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,116 @@
 
-import { useEffect, useState } from 'react'
+// import { useEffect, useState } from 'react'
+// import './App.css'
+
+// function App() {
+  
+//   const [users, setUsers] = useState ([]);
+
+//   useEffect(()=>{
+//     fetch ('http://localhost:5000/users')
+//     .then(res => res.json())
+//     .then(data => setUsers(data));
+//   }, [])
+
+
+// const handleAddUser = event =>{
+// event.preventDefault();
+// const form =event.target;
+// const name =form.Name.value;
+// const email=form.Email.value;
+// const user ={ name ,email}
+// console.log(user);
+// fetch('http://localhost:5000/users',{
+//   method: 'POST',
+//   headers: {
+//     'content-type' : "application/json"
+//   },
+//   body:JSON.stringify(user)
+// } )
+// .then(res => res.json())
+// .then(data => {
+//   console.log(data);
+  
+// const newUsers = [... users, data]
+// setUsers(newUsers);
+// form.reset();
+
+// })
+
+//   }
+
+//   return (
+//     <>
+      
+//       <h1>User Manegment Clientsite</h1>
+//       <p>Number of Users: {users.length}</p>
+
+// <form onSubmit={handleAddUser}>
+//   <input type="text" name="Name" id="" />
+//   <br />
+//   <input type="email" name="Email" id="" />
+//   <br />
+//   <input type="submit" name="add user" id="" />
+ 
+// </form>
+
+//       <div>
+//         {
+//           users.map(user => <p key={user.id}> {user.id} {user.name} - {user.email}</p>)
+//         }
+//       </div>
+
+//     </>
+//   )
+// }
+
+// export default App
+
+
+
+
 import './App.css'
 
-function App() {
-  
-  const [users, setUsers] = useState ([]);
-
-  useEffect(()=>{
-    fetch ('http://localhost:5000/users')
-    .then(res => res.json())
-    .then(data => setUsers(data));
-  }, [])
-
+function App(){
 
 const handleAddUser = event =>{
 event.preventDefault();
-const form =event.target;
-const name =form.Name.value;
-const email=form.Email.value;
-const user ={ name ,email}
+const form = event.target;
+const name = form.name.value;
+const email = form.email.value;
+const user = {name, email};
 console.log(user);
-fetch('http://localhost:5000/users',{
-  method: 'POST',
-  headers: {
-    'content-type' : "application/json"
-  },
-  body:JSON.stringify(user)
-} )
+
+
+fetch('http://localhost:5000/users', {
+method: 'POST',
+headers: {
+  'content-type' : 'application/json'
+},
+body: JSON.stringify(user)
+})
 .then(res => res.json())
 .then(data => {
   console.log(data);
-  
-const newUsers = [... users, data]
-setUsers(newUsers);
-form.reset();
-
 })
-
-  }
-
-  return (
+}
+  return(
     <>
-      
-      <h1>User Manegment Clientsite</h1>
-      <p>Number of Users: {users.length}</p>
+    <h1>Hello</h1>
 
 <form onSubmit={handleAddUser}>
-  <input type="text" name="Name" id="" />
+  <input type='text' name='name' id='' />
   <br />
-  <input type="email" name="Email" id="" />
+  <input type='email' name='email' id='' />
   <br />
-  <input type="submit" name="add user" id="" />
- 
+  <input type='submit' name='submit' id='' />
+  
 </form>
 
-      <div>
-        {
-          users.map(user => <p key={user.id}> {user.id} {user.name} - {user.email}</p>)
-        }
-      </div>
+
 
     </>
   )
 }
 
-export default App
+export default App;
